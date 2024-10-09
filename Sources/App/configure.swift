d4a93a8419b44c6e83719dc4bf59866ce032d40b
Fortune David <fortunekbz2009@gmail.com>
2024-10-10 00:33:08 +0100

    configured heroku database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,14 +7,24 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    if let urlString = Environment.get("DATABASE_HOST"),
-       var postgresConfig = PostgresConfiguration(url: urlString)
-       {
-        var tlsConfig = TLSConfiguration.makeClientConfiguration()
-        tlsConfig.certificateVerification = .none
-        postgresConfig.tlsConfiguration = tlsConfig
-        
-        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+//    if let urlString = Environment.get("DATABASE_HOST"),
+//       var postgresConfig = PostgresConfiguration(url: urlString)
+//       {
+//        var tlsConfig = TLSConfiguration.makeClientConfiguration()
+//        tlsConfig.certificateVerification = .none
+//        postgresConfig.tlsConfiguration = tlsConfig
+//        
+//        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    
+    if let databaseURL = Environment.get("DATABASE_URL") {
+    var tlsConfig: TLSConfiguration = .makeClientConfiguration()
+       tlsConfig.certificateVerification = .none
+       let nioSSLContext = try NIOSSLContext(configuration: tlsConfig)
+
+       var postgresConfig = try SQLPostgresConfiguration(url: databaseURL)
+       postgresConfig.coreConfiguration.tls = .require(nioSSLContext)
+
+       app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
     }else{
         app.databases.use(.postgres(
             hostname: Environment.get("DATABASE_HOST") ?? "localhost",
